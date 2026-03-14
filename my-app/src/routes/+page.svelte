9970ab409f4edac4dcb581d6453ea226a5d40dc2
Planta703 <amuqtk@gmail.com>
2026-03-14 19:19:23 +0000

    checking & adding

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    interface Item {
+        id: string;
+        checked: boolean;
+    }
+    
+    let input = $state('')
+    let list = $state<Item[]>([])
+    
+    function check(item: Item) {
+        item.checked = !item.checked
+        list = list;
+    }
+    
+    function itemsToList(e: KeyboardEvent) {
+        if (e.key !== 'Enter') return
+        
+        e.preventDefault()
+        if (!input.trim()) return
+        
+        list.push({id: input, checked: false})
+        input=''
+    }
+</script>
+<input type="text" class="input" onkeypress={itemsToList} contenteditable="true" bind:value={input}>
+<ol class="list">
+    {#each list as item (item.id)}
+    <li class="list-row">
+        <input type="checkbox" class="checkbox" onclick={() => check(item)}><p class="{item.checked ? 'line-through': ''}">{item.id}</p>
+    </li>
+    {/each}
+</ol>
