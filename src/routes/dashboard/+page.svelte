858a1d382ef1feedb2c5c324bd4a9224cfb96bff
Planta703 <amuqtk@gmail.com>
2026-03-20 08:25:48 -0500

    dashboard redirect

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -37,6 +37,15 @@
         const { data: { user } } = await supabase.auth.getUser()
         if (!user) {
             window.location.href = '/'
+        } else {
+            const { data: userData } = await supabase
+            .from('users')
+            .select('*')
+            .eq("type", "dashboard")
+            .eq("user_id", currentUserId)
+            if (!userData || !userData.length > 0) {
+                window.location.href = '/'
+            }
         }
     }
     
